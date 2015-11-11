346b87f5145f8f36c374021b30c62f71ba5d8cef
王政 <wangzheng4@staff.weibo.com>
2015-11-11 13:53:57 +0800

    rm server_front

--- HelloWorld/server_front/js/components/index/logic.js
+++
@@ -1,9 +0,0 @@
-steel.d("components/index/logic", [],function(require, exports, module) {
-/**
- * 模块逻辑
- */
-
-module.exports = function(node) {
-	node.querySelector('h1').style.color = "#f00";
-};
-});
